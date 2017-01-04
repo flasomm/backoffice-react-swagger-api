c1296ac78dc8006efc011e1c5865d1ec99d0a745
Fabrice Sommavilla <flasomm@gmail.com>
2017-01-05 00:29:17 +0100

    update datagrid

--- client/containers/Profiles/index.js
+++ client/containers/Profiles/index.js
@@ -17,6 +17,10 @@ export default class Profiles extends Component {
         this.state = {profiles: []};
     }
 
+    /**
+     *
+     * @param cb
+     */
     getProfilesData(cb) {
         fetch(auth.getServerUrl() + '/users?api_key=' + auth.getToken(), {
             method: 'GET',
@@ -41,20 +45,108 @@ export default class Profiles extends Component {
 
     /**
      *
+     * @param onClick
+     */
+    handleDeleteButtonClick = (onClick) => {
+        onClick();
+    }
+
+    /**
+     *
+     * @param onClick
+     */
+    handleInsertButtonClick = (onClick) => {
+        onClick();
+    }
+
+    /**
+     *
+     * @param onClick
+     * @returns {XML}
+     */
+    createDeleteButton = (onClick) => {
+        return (
+            <DeleteButton
+                btnText='CustomDeleteText'
+                btnContextual='btn-success'
+                className='my-custom-class'
+                btnGlyphicon='glyphicon-edit'
+                onClick={ e => this.handleDeleteButtonClick(onClick) }/>
+        );
+    }
+
+    /**
+     *
+     * @param onClick
+     * @returns {XML}
+     */
+    createInsertButton = (onClick) => {
+        return (
+            <InsertButton
+                btnText='CustomInsertText'
+                btnContextual='btn-warning'
+                className='my-custom-class'
+                btnGlyphicon='glyphicon-edit'
+                onClick={ () => this.handleInsertButtonClick(onClick) }/>
+        );
+    }
+
+    /**
+     *
+     * @param cell
+     * @returns {string}
+     */
+    genderFormatter(cell) {
+        return cell === "m" ? "male" : "female";
+    }
+
+    /**
+     *
+     * @param cell
+     * @param row
+     * @param enumObject
+     * @param index
+     * @returns {XML}
+     */
+    actionsFormatter(cell, row, enumObject, index) {
+        return (
+            <a href={cell}><span className="glyphicon glyphicon-pencil"></span></a>
+        );
+    }
+
+    /**
+     *
      * @returns {XML}
      */
     render() {
+        const options = {
+            deleteBtn: this.createDeleteButton,
+            insertBtn: this.createInsertButton
+        };
+        const selectRowProp = {
+            mode: 'checkbox'
+        };
         return (
             <div className="container">
                 <div className="row">
                     <h1>Profiles</h1>
-                    <BootstrapTable data={ this.state.profiles } pagination>
-                        <TableHeaderColumn dataField='email' isKey={ true }>Email</TableHeaderColumn>
+                    <hr/>
+                    <BootstrapTable
+                        data={ this.state.profiles }
+                        selectRow={ selectRowProp }
+                        options={ options }
+                        deleteRow
+                        insertRow
+                        pagination
+                        search
+                    >
+                        <TableHeaderColumn dataField='email' isKey={true}>Email</TableHeaderColumn>
                         <TableHeaderColumn dataField='username'>Username</TableHeaderColumn>
                         <TableHeaderColumn dataField='firstname'>Firstname</TableHeaderColumn>
                         <TableHeaderColumn dataField='lastname'>Lastname</TableHeaderColumn>
-                        <TableHeaderColumn dataField='gender'>Gender</TableHeaderColumn>
+                        <TableHeaderColumn dataField='gender' dataFormat={this.genderFormatter}>Gender</TableHeaderColumn>
                         <TableHeaderColumn dataField='group'>Group</TableHeaderColumn>
+                        <TableHeaderColumn dataField='_id' dataFormat={ this.actionsFormatter }></TableHeaderColumn>
                     </BootstrapTable>
                 </div>
             </div>
