989019673042ac647cf84019d9a688420ece2386
Fabrice Sommavilla <flasomm@gmail.com>
2017-01-24 00:28:37 +0100

    edit profile wip

--- client/containers/Profile/index.js
+++ client/containers/Profile/index.js
@@ -8,6 +8,8 @@
 "use strict";
 
 import React, {Component} from 'react';
+import Helmet from 'react-helmet';
+import {Footer, Input} from 'components';
 import auth from '../utils/auth'
 import 'whatwg-fetch';
 import dateFormat from 'dateformat';
@@ -23,7 +25,7 @@ export default class Profiles extends Component {
      */
     constructor(props) {
         super(props);
-        this.state = {profile: [], message: ""};
+        this.state = {profile: {}, message: ""};
     }
 
     /**
@@ -55,8 +57,34 @@ export default class Profiles extends Component {
         });
     }
 
+    handleChange(value, key) {
+        var state = {};
+        state.profile[key] = value;
+        this.setState(state);
+    }
+
+    /**
+     *
+     * @returns {*}
+     */
     displayPageTitle() {
-        return this.props.params.id === auth.getUserId() ? "Your Profile" : `Profile: ${this.state.profile.firstname} ${this.state.profile.lastname}`;
+        if (this.props.params.id === auth.getUserId()) {
+            return "Your Profile";
+        }
+
+        return `Profile: ${this.state.profile.firstname} ${this.state.profile.lastname}`;
+    }
+
+    /**
+     *
+     * @returns {XML}
+     */
+    renderMessage() {
+        if (this.state.message.length > 0) {
+            return (
+                <div className={'alert alert-' + this.state.type} role="alert">{this.state.message}</div>
+            );
+        }
     }
 
     /**
@@ -64,34 +92,102 @@ export default class Profiles extends Component {
      * @returns {XML}
      */
     render() {
+        const styles = require('./styles.css');
         return (
             <div className="container">
-                <div className="row">
+                <Helmet title={this.displayPageTitle()}/>
+                <div className="page-header">
                     <h1>{this.displayPageTitle()}</h1>
-                    <hr/>
-                    <dl className="dl-horizontal">
-                        <dt>Email</dt>
-                        <dd>{this.state.profile.email}</dd>
-                        <dt>Username</dt>
-                        <dd>{this.state.profile.username}</dd>
-                        <dt>First Name</dt>
-                        <dd>{this.state.profile.firstname}</dd>
-                        <dt>Last Name</dt>
-                        <dd>{this.state.profile.lastname}</dd>
-                        <dt>Gender</dt>
-                        <dd>{this.state.profile.gender === 'm' ? 'Male' : 'Female'}</dd>
-                        <dt>Group</dt>
-                        <dd>{this.state.profile.group}</dd>
-                        <dt>Active</dt>
-                        <dd>{this.state.profile.active ? "Yes" : "No"}</dd>
-                        <dt>Created At</dt>
-                        <dd>{dateFormat(this.state.profile.createdAt, "dd/mm/yyyy, HH:MM:ss")}</dd>
-                        <dt>Updated At</dt>
-                        <dd>{dateFormat(this.state.profile.updatedAt, "dd/mm/yyyy, HH:MM:ss")}</dd>
-                    </dl>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <span className="panel-title">Basic Form Elements</span>
+                            </div>
+                            <div className="panel-body">
+                                <div className="row">
+                                    <div className="col-lg-6">
+                                        <div className="formEditProfile">
+                                            {this.state.profile.group}
+                                            {this.state.profile.active ? "Yes" : "No"}
+                                            {this.state.profile.gender === 'm' ? 'Male' : 'Female'}
+                                            {dateFormat(this.state.profile.createdAt, "dd/mm/yyyy, HH:MM:ss")}
+                                            {dateFormat(this.state.profile.updatedAt, "dd/mm/yyyy, HH:MM:ss")}
+                                            <form id="loginForm" name="loginForm" onSubmit={this.onSubmit}>
+                                                <div className="form-group">
+                                                    <label htmlFor="email" className="control-label">Email address</label>
+                                                    <Input
+                                                        id="email"
+                                                        type="email"
+                                                        name="email"
+                                                        ref="email"
+                                                        className="form-control"
+                                                        value={this.state.profile.email}
+                                                        required={true}
+                                                        placeholder="Your email"
+                                                        maxLength="100"
+                                                        onChange={this.handleChange}
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="username" className="control-label">UserName</label>
+                                                    <Input
+                                                        id="username"
+                                                        type="username"
+                                                        name="username"
+                                                        ref="username"
+                                                        className="form-control"
+                                                        value={this.state.profile.username}
+                                                        required={true}
+                                                        placeholder="Your username"
+                                                        maxLength="100"
+                                                        onChange={this.handleChange}
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="firstname" className="control-label">First Name</label>
+                                                    <Input
+                                                        id="firstname"
+                                                        type="firstname"
+                                                        name="firstname"
+                                                        ref="firstname"
+                                                        className="form-control"
+                                                        value={this.state.profile.firstname}
+                                                        required={true}
+                                                        placeholder="Your firstname"
+                                                        maxLength="100"
+                                                        onChange={this.handleChange}
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label htmlFor="lastname" className="control-label">Last Name</label>
+                                                    <Input
+                                                        id="lastname"
+                                                        type="lastname"
+                                                        name="lastname"
+                                                        ref="lastname"
+                                                        className="form-control"
+                                                        value={this.state.profile.lastname}
+                                                        required={true}
+                                                        placeholder="Your lastname"
+                                                        maxLength="100"
+                                                        onChange={this.handleChange}
+                                                    />
+                                                </div>
+                                                { this.renderMessage() }
+                                                <button type="submit" className="btn btn-primary">Save</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
+        );
+
     }
 
 
